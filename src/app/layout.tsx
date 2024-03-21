51ad5041a03b3010524883945865b9bbf1368c81
kwonhygge <cleancode.kwon@gmail.com>
2024-03-21 22:17:09 +0900

    feat: first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Archivo_Black, Manrope } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
+const archivoBlack = Archivo_Black({
+  subsets: ["latin"],
+  style: "normal",
+  weight: "400",
+});
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  style: "normal",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ko">
+      <body className={manrope.className}>
+        <header
+          className={`${archivoBlack.className} flex justify-center items-center min-h-20 text-xl md:text-2xl tracking-tighter`}
+        >
+          <Link href="/">NAHEE PARK</Link>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,70 @@
 import Image from "next/image";
+import Link from "next/link";
+import Cover1 from "@/static/images/1/cover.jpg";
+import Cover2 from "@/static/images/2/01-255-01-cover.jpg";
+import Cover3 from "@/static/images/3/01-670-01-cover.jpg";
+import Cover4 from "@/static/images/4/01-387-01-cover.jpg";
+import Cover5 from "@/static/images/5/01-721-01-cover.jpg";
+import Cover6 from "@/static/images/6/02-602-01-cover.jpg";
+import Cover7 from "@/static/images/7/01-904-01-cover.jpg";
+import Cover9 from "@/static/images/9/A-36-01-cover.jpg";
+import Cover10 from "@/static/images/10/A-30-2-03-cover.jpg";
+import Cover11Temp from "@/static/images/11/01-586-02.jpg";
+import Cover12 from "@/static/images/12/6.jpg";
+import Cover13 from "@/static/images/13/01-268-1-cover.jpg";
+import Cover14 from "@/static/images/14/cover.jpg";
+import Cover15 from "@/static/images/15/01-021-cover.jpg";
+import Cover16 from "@/static/images/16/IMG_7066-cover.png";
+import Cover17 from "@/static/images/17/cover.jpg";
+import Cover18 from "@/static/images/18/C-53-1-cover.jpg";
+import Cover19 from "@/static/images/19/cover.jpg";
+import Cover24 from "@/static/images/24/01-165.jpg";
+
+const MAIN_IMAGE_LIST = [
+  { src: Cover1, alt: "main-image-1" },
+  { src: Cover2, alt: "main-image-2" },
+  { src: Cover3, alt: "main-image-3" },
+  { src: Cover4, alt: "main-image-4" },
+  { src: Cover5, alt: "main-image-5" },
+  { src: Cover6, alt: "main-image-6" },
+  { src: Cover7, alt: "main-image-7" },
+  { src: Cover9, alt: "main-image-9" },
+  { src: Cover10, alt: "main-image-10" },
+  { src: Cover11Temp, alt: "main-image-11" },
+  { src: Cover12, alt: "main-image-12" },
+  { src: Cover13, alt: "main-image-13" },
+  { src: Cover14, alt: "main-image-14" },
+  { src: Cover15, alt: "main-image-15" },
+  { src: Cover16, alt: "main-image-16" },
+  { src: Cover17, alt: "main-image-17" },
+  { src: Cover18, alt: "main-image-18" },
+  { src: Cover19, alt: "main-image-19" },
+  { src: Cover24, alt: "main-image-24" },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className="flex min-h-screen w-screen flex-col items-center">
+      <ul className="grid grid-cols-1 md:grid-cols-4 gap-4 px-6 md:px-8 w-full">
+        {MAIN_IMAGE_LIST.map((image, index) => {
+          return (
+            <li key={index} className="w-full mb-4 lg:mb-0">
+              <Link href={`/list/${index + 1}`}>
+                <div className="w-full relative pb-[133%]">
+                  <Image
+                    src={image.src}
+                    alt={image.alt}
+                    fill
+                    className={"object-cover"}
+                  />
+                </div>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+      <div className={"text-sm h-96 flex justify-center items-center"}>
+        All rights reserved. ©2024
       </div>
     </main>
   );
