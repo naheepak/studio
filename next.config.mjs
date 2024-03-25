83c70611f2a5c44fcb22012c18c72ffb69027e79
naheepak <164845716+naheepak@users.noreply.github.com>
2024-03-25 15:22:20 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: '/nahee-2024',
+    assetPrefix: '/evbt',
     images:{
         unoptimized: true
     },
