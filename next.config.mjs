126fa5163269204edbbb656cabc2ce5aeab517c4
kwonhygge <cleancode.kwon@gmail.com>
2024-03-29 21:27:56 +0900

    test2

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: process.env.NODE_ENV === 'development' ? '' : '/evbt',
     output: process.env.NODE_ENV === 'development' ? 'standalone' : 'export',
     assetPrefix: process.env.NODE_ENV === 'development' ? '' : '/evbt',
     images:{
