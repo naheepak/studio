a38d26ec37dff5d07ca0166f8957770f1dee4a0f
kwonhygge <cleancode.kwon@gmail.com>
2024-03-31 02:05:28 +0900

    test: image optimization 추가

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
     basePath: process.env.NODE_ENV === 'development' ? '' : '/studio',
     output: process.env.NODE_ENV === 'development' ? 'standalone' : 'export',
     assetPrefix: process.env.NODE_ENV === 'development' ? '' : '/studio',
-    images:{
-        unoptimized: true
-    },
 };
 
 export default nextConfig;
