6bf452430ae8b0ecb892d7726055481f275fdb6e
kwonhygge <cleancode.kwon@gmail.com>
2024-06-26 22:55:32 +0900

    feat: 이미지 추가

--- src/app/02/contents/cover-image-list.ts
+++ src/app/02/contents/cover-image-list.ts
@@ -2,6 +2,7 @@ import Cover1 from "@/static/images/02/1/IMG_6579 cover.webp";
 import Cover2 from "@/static/images/02/2/IMG_2860 cover.webp";
 import Cover3 from "@/static/images/02/3/IMG_3224 cover.webp";
 import Cover4 from "@/static/images/02/4/PTG_POSTER_japan cover.webp";
+import Cover5 from "@/static/images/02/5/JIMBIM2.webp";
 
 import { CoverImage } from "../../../interfaces";
 
@@ -24,6 +25,11 @@ export const COVER_IMAGE_LIST: CoverImage[] = [
   {
     src: Cover4,
     id: "4",
+    disabled: true,
+  },
+  {
+    src: Cover5,
+    id: "5",
     disabled: true,
   },
 ];
